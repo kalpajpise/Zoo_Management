d4f3fefdf532c6c1c324545df17caf050fcac24f
kalpajpise <kalpaj12@gmail.com>
2019-01-16 22:47:38 +0530

    completed

---
+++ src/sampleconnection/ZMS1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sampleconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author bablo
+ */
+public class ZMS1 {
+  
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Addemp nj = new Addemp();
+        nj.setVisible(true);
+        nj.pack();
+
+       
+    }
+    
+}
